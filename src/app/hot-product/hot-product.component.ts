3bc047534aee5ef21416bc56505907c235dbc402
thanhdvph06975 <demonstyle30@gmail.com>
2020-03-28 00:42:12 +0700

    3 files modified

--- src/app/hot-product/hot-product.component.ts
+++ src/app/hot-product/hot-product.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { product } from "../product";
+import { ProductService } from "../product.service";
 @Component({
-  selector: 'app-hot-product',
-  templateUrl: './hot-product.component.html',
-  styleUrls: ['./hot-product.component.css']
+  selector: "app-hot-product",
+  templateUrl: "./hot-product.component.html",
+  styleUrls: ["./hot-product.component.css"]
 })
 export class HotProductComponent implements OnInit {
-
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
+    this.getProducts();
   }
-
+  products: product[];
+  getProducts() {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+    });
+  }
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,12 +6,14 @@ import { Observable } from "rxjs";
 export class ProductService {
   api = "https://5e7b1af40e04630016332b23.mockapi.io/product";
   constructor(private http: HttpClient) {}
+
   getProducts(): Observable<product[]> {
     return this.http.get<product[]>(this.api);
   }
-  
+
   getProduct(id): Observable<product> {
     // console.log(this.api+"/"+id);
     return this.http.get<product>(this.api + "/" + id);
   }
+  
 }
