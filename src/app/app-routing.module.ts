08b34764facd4da686a20a3c49986e878408cde0
thanhdvph06975 <demonstyle30@gmail.com>
2020-04-18 22:25:37 +0700

    9 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,39 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ProductManagerComponent } from './product-manager/product-manager.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ErrorComponent } from './error/error.component';
-import { AdminComponent } from './backend/admin/admin.component';
-import { IndexComponent } from './frontend/index/index.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { ProductManagerComponent } from "./product-manager/product-manager.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { ErrorComponent } from "./error/error.component";
+import { AdminComponent } from "./backend/admin/admin.component";
+import { IndexComponent } from "./frontend/index/index.component";
 
 const routes: Routes = [
-  { path: '', redirectTo:'home',pathMatch:"full" },
-  { path: 'home', component: HomeComponent },
-  { path: 'admin', component: AdminComponent,
-  children:[
-    { path: 'manager', component: ProductManagerComponent }
-  ]
-   },
-   { path: 'index', component: IndexComponent },
-  { path: 'manager', component: ProductManagerComponent },
-  { path: 'product/:id', component: ProductDetailComponent },
-  { path: 'error', component: ErrorComponent },
-  { path: '**', redirectTo:'error',pathMatch:"full" },
+  { path: "", redirectTo: "index", pathMatch: "full" },
+  {
+    path: "index",
+    component: IndexComponent,
+    children: [
+      { path: "", redirectTo: "home", pathMatch: "full" },
+      { path: "home", component: HomeComponent },
+      { path: "product/:id", component: ProductDetailComponent }
+    ]
+  },
+  {
+    path: "admin",
+    component: AdminComponent,
+    children: [
+      { path: "", redirectTo: "manager", pathMatch: "full" },
+      { path: "manager", component: ProductManagerComponent },
+      { path: "product/:id", component: ProductDetailComponent }
+    ]
+  },
+  { path: "manager", component: ProductManagerComponent },
+  { path: "error", component: ErrorComponent },
+  { path: "**", redirectTo: "error", pathMatch: "full" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
